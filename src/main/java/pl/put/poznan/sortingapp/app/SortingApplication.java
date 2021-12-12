165c343e7484079888109a1b3b777b4db1360e56
Tomasz Pieścikowski <tomek.piescikowski@gmail.com>
2021-12-12 14:25:54 +0100

    zmiana nazw plikow aplikacji i zakomentowanie tymczasowe zeby sie kompilowalo

---
+++ src/main/java/pl/put/poznan/sortingapp/app/SortingApplication.java
@@ -0,0 +1,29 @@
+package pl.put.poznan.sortingapp.app;
+
+import pl.put.poznan.sortingapp.logic.MergeSort;
+import static pl.put.poznan.sortingapp.logic.MergeSort.printArray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//zakomentowane chwilowo
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.sortingapp.rest"})
+public class SortingApplication {
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        SpringApplication.run(pl.put.poznan.sortingapp.app.SortingApplication.class, args);
+        int[] arr = { 12, 11, 13, 5, 6, 7 };
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
