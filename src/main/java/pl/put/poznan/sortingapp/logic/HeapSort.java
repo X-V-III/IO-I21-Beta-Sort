f28ba724c6451deab2cf0bcc176153f22bd17f63
LenaPtak <lenkaptak@gmail.com>
2021-12-16 13:16:17 +0100

    Próby HeapSort

--- src/main/java/pl/put/poznan/sortingapp/logic/HeapSort.java
+++ src/main/java/pl/put/poznan/sortingapp/logic/HeapSort.java
@@ -1,4 +1,68 @@
 package pl.put.poznan.sortingapp.logic;
 
-public class HeapSort {
+
+public class HeapSort
+{
+    private static int[] a;
+    private static int n;
+    private static int left;
+    private static int right;
+    private static int largest;
+
+    // Build-Heap Function
+    public static void buildheap(int []a){
+        n=a.length-1;
+        for(int i=n/2;i>=0;i--){
+            //System.out.println(i);
+            maxheap(a,i);
+        }
+    }
+
+    // Max-Heap Function
+    public static void maxheap(int[] a, int i){
+        left=2*i;
+        right=2*i+1;
+        //System.out.println(i + " " + left + " " + right);
+        if(left <= n && a[left] > a[i]){
+            largest=left;
+        }
+        else{
+            largest=i;
+        }
+
+        if(right <= n && a[right] > a[largest]){
+            largest=right;
+        }
+        if(largest!=i){
+            exchange(i,largest);
+            maxheap(a, largest);
+        }
+    }
+
+    // Exchange Function
+    public static void exchange(int i, int j){
+        int t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+    }
+
+    // Sort Function
+    public static void sort(int []a0){
+        a=a0;
+        buildheap(a);
+
+        for(int i=n;i>0;i--){
+            exchange(0, i);
+            n=n-1;
+            maxheap(a, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int []a1={10,9,8,7,6,5,4,3,2,1};
+        sort(a1);
+        for(int i=0;i<a1.length;i++){
+            System.out.print(a1[i] + " ");
+        }
+    }
 }
