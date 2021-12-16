aa8ca784815502059b1b73303443810fde23157e
XVIII <akamilive@gmail.com>
2021-12-16 14:38:30 +0100

    Implemented insertion sort and added class for utility functions

--- src/main/java/pl/put/poznan/sortingapp/logic/InsertionSort.java
+++ src/main/java/pl/put/poznan/sortingapp/logic/InsertionSort.java
@@ -1,4 +1,33 @@
 package pl.put.poznan.sortingapp.logic;
 
+import pl.put.poznan.sortingapp.util.Utils;
+
 public class InsertionSort {
+    /**
+     * Sorts given array of integers with insertion sort
+     * @param arr Unsorted array of integers
+     */
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    /**
+     * Gives a quick demonstration on a sample array
+     */
+    public static void quickTest() {
+        int[] testArr = {4,3,2,1};
+        System.out.println("Unsorted: ");
+        Utils.printArray(testArr);
+        sort(testArr);
+        System.out.println("After insertion sort: ");
+        Utils.printArray(testArr);
+    }
 }
---
+++ src/main/java/pl/put/poznan/sortingapp/util/Utils.java
@@ -0,0 +1,12 @@
+package pl.put.poznan.sortingapp.util;
+
+public class Utils {
+    /**
+     * Prints array of int
+     * @param arr Array to print
+     */
+    public static void printArray(int[] arr) {
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+}
