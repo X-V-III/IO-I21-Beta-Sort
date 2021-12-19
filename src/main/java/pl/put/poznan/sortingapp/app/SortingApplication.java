d4be6386236d7a6407f9e8f5368ea5905489bb33
Tomasz Pieścikowski <63928070+tomaszpiescikowski@users.noreply.github.com>
2021-12-19 10:27:47 +0100

    Update SortingApplication.java
    tetsowy blad

--- src/main/java/pl/put/poznan/sortingapp/app/SortingApplication.java
+++ src/main/java/pl/put/poznan/sortingapp/app/SortingApplication.java
@@ -1,81 +1,81 @@
-package pl.put.poznan.sortingapp.app;
+// package pl.put.poznan.sortingapp.app;
 
-import pl.put.poznan.sortingapp.logic.*;
+// import pl.put.poznan.sortingapp.logic.*;
 
-import static pl.put.poznan.sortingapp.util.Utils.printArray;
+// import static pl.put.poznan.sortingapp.util.Utils.printArray;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-/**
- * Klasa SortingApplication zawierajaca glowna metode rozruchowa main.
- *
- * @author
- * @version 1.0
- */
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.sortingapp.rest"})
-public class SortingApplication {
+// /**
+//  * Klasa SortingApplication zawierajaca glowna metode rozruchowa main.
+//  *
+//  * @author
+//  * @version 1.0
+//  */
+// @SpringBootApplication(scanBasePackages = {"pl.put.poznan.sortingapp.rest"})
+// public class SortingApplication {
 
-    /**
-     * Glowna metoda rozruchowa aplikacji. Rozpoczyna prace serwera z pomoca biblioteki Spring. Inicjuje obiekty potrzebne do dzialania.
-     *
-     * @param args Domyslny parametr metody main().
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(pl.put.poznan.sortingapp.app.SortingApplication.class, args);
-        int[] arr = {12, 11, 13, 5, 6, 7};
-        int[] arrHeap = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        int[] arrBubble = {5, 6, 2, 1, 4};
-        int[] arrQuick = {24, 5, 3, 35, 14, 23, 19, 43, 2};
+//     /**
+//      * Glowna metoda rozruchowa aplikacji. Rozpoczyna prace serwera z pomoca biblioteki Spring. Inicjuje obiekty potrzebne do dzialania.
+//      *
+//      * @param args Domyslny parametr metody main().
+//      */
+//     public static void main(String[] args) {
+//         SpringApplication.run(pl.put.poznan.sortingapp.app.SortingApplication.class, args);
+//         int[] arr = {12, 11, 13, 5, 6, 7};
+//         int[] arrHeap = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+//         int[] arrBubble = {5, 6, 2, 1, 4};
+//         int[] arrQuick = {24, 5, 3, 35, 14, 23, 19, 43, 2};
 
-        int lowQuick = 0;
-        int highQuick = arrQuick.length - 1;
+//         int lowQuick = 0;
+//         int highQuick = arrQuick.length - 1;
 
-        try {
-            //Merge Sort
-            System.out.println("Merge Sort Given Array");
-            printArray(arr);
+//         try {
+//             //Merge Sort
+//             System.out.println("Merge Sort Given Array");
+//             printArray(arr);
 
-            MergeSort ob = new MergeSort();
-            ob.sort(arr, 0, arr.length - 1);
+//             MergeSort ob = new MergeSort();
+//             ob.sort(arr, 0, arr.length - 1);
 
-            System.out.println("\nMerge Sort Sorted Array");
-            printArray(arr);
+//             System.out.println("\nMerge Sort Sorted Array");
+//             printArray(arr);
 
-            //HeapSort
-            System.out.println("\nHeap Sort Given Array");
-            printArray(arrHeap);
+//             //HeapSort
+//             System.out.println("\nHeap Sort Given Array");
+//             printArray(arrHeap);
 
-            HeapSort.sort(arrHeap);
+//             HeapSort.sort(arrHeap);
 
-            System.out.println("\nHeap Sort Sorted Array");
-            printArray(arrHeap);
+//             System.out.println("\nHeap Sort Sorted Array");
+//             printArray(arrHeap);
 
-            //BubbleSort
-            System.out.println("\nBubble Sort Given Array");
-            printArray(arrBubble);
+//             //BubbleSort
+//             System.out.println("\nBubble Sort Given Array");
+//             printArray(arrBubble);
 
-            BubbleSort.sort(arrBubble);
+//             BubbleSort.sort(arrBubble);
 
-            System.out.println("\nBubble Sort Sorted Array");
-            printArray(arrBubble);
+//             System.out.println("\nBubble Sort Sorted Array");
+//             printArray(arrBubble);
 
-            //QuickSort
-            System.out.println("\nQuick Sort Given Array");
-            printArray(arrQuick);
+//             //QuickSort
+//             System.out.println("\nQuick Sort Given Array");
+//             printArray(arrQuick);
 
-            QuickSort.sort(arrQuick, lowQuick, highQuick);
+//             QuickSort.sort(arrQuick, lowQuick, highQuick);
 
-            System.out.println("\nQuick Sort Sorted Array");
-            printArray(arrQuick);
+//             System.out.println("\nQuick Sort Sorted Array");
+//             printArray(arrQuick);
 
-            //Empty list exception
-            InsertionSort.sort(new int[]{});
-        } catch (IllegalArgumentException e) {
-            System.out.println("\nGiven array is empty");
-            e.printStackTrace();
-        }
+//             //Empty list exception
+//             InsertionSort.sort(new int[]{});
+//         } catch (IllegalArgumentException e) {
+//             System.out.println("\nGiven array is empty");
+//             e.printStackTrace();
+//         }
 
-    }
-}
+//     }
+// }
