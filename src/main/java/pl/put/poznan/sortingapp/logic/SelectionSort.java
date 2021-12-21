9c853c1e0b26bc175d22b6f7e74eb30eef89d2b6
lilitka7 <79264274+lilitka7@users.noreply.github.com>
2021-12-21 16:10:43 +0100

    Update SelectionSort.java
    Sortuje, ale nie wypisuje 
    (niedokończone)

--- src/main/java/pl/put/poznan/sortingapp/logic/SelectionSort.java
+++ src/main/java/pl/put/poznan/sortingapp/logic/SelectionSort.java
@@ -11,26 +11,24 @@ public class SelectionSort {
      * @throws IllegalArgumentException Kiedy podany ciag jest pusty badz niepoprawny, konczymy bledem.
      *
      */
-    public static void sort(int[] arr) throws IllegalArgumentException {
+    public static void sort(String[] arr, String sortAs) throws IllegalArgumentException {
 
         // Throws an exception when arr is empty
         if (arr.length == 0) throw new IllegalArgumentException();
 
-        for (int i = 0; i < arr.length-1; i++)
-        {
-            int min_index = i;
-
-            for (int j = i+1; j < arr.length; j++)
-                if (arr[j] < arr[min_index])
-                    min_index = j;
-
-            int temp = arr[min_index];
-            arr[min_index] = arr[i];
-            arr[i] = temp;
+        if (sortAs.compareToIgnoreCase("String") == 0) {
+            sortAsStrings(arr);
+        } else if (sortAs.compareToIgnoreCase("Integer") == 0) {
+            sortAsIntegers(arr);
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 
-    /////////////////////////////////
+    /**
+     * Sorts arrray of Strings as Strings
+     */
+
 
     /**
      * Sortuje podany ciag obiektow algorytmem SelectionSort typu String.
@@ -38,7 +36,7 @@ public class SelectionSort {
      * @throws IllegalArgumentException Kiedy podany ciag jest pusty badz niepoprawny, konczymy bledem.
      *
      */
-    public static void sortString(String[] elements) throws IllegalArgumentException {
+    public static void sortAsStrings(String[] elements) throws IllegalArgumentException {
         selectionSort(elements,0);
     }
 
@@ -63,5 +61,18 @@ public class SelectionSort {
         String tmp = ele[i];
         ele[i] = ele[j];
         ele[j] = tmp;
+    }
+    private static void sortAsIntegers(String[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_index = i;
+
+            for (int j = i + 1; j < arr.length; j++)
+                if (Integer.parseInt(arr[j]) < Integer.parseInt(arr[min_index]))
+                    min_index = j;
+
+            String temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
