7a246999a7123b7538cf05f7de475676a0132260
lilitka7 <79264274+lilitka7@users.noreply.github.com>
2021-12-21 16:09:55 +0100

    Update QuickSort.java
    Sortuje, ale nie wypisuje 
    (niedokończone)

--- src/main/java/pl/put/poznan/sortingapp/logic/QuickSort.java
+++ src/main/java/pl/put/poznan/sortingapp/logic/QuickSort.java
@@ -1,56 +1,40 @@
 package pl.put.poznan.sortingapp.logic;
 
-import java.util.Arrays;
-
 /**
  * Klasa bedaca implementacja algorytmu sortujacego QuickSort.
  */
 public class QuickSort {
 
     /**
-     * Sortuje podany ciag obiektow algorytmem QuickSort.
+     * Sortuje podany ciag obiektow typu String algorytmem QuickSort.
+     *
      * @param array Nieposortowany ciag obiektow.
-     * @param low Pierwszy wartosc.
-     * @param high Druga wartosc.
+     * @param low   Pierwsza wartosc.
+     * @param high  Ostatnia wartosc.
+     * @param sortAs Sortować elementy jako obiekty jakiej klasy.
      */
-    public static void sort(int[] array, int low, int high) {
+    public static void sort(String[] array, int low, int high, String sortAs) {
         if (array.length == 0)
             return;
 
         if (low >= high)
             return;
-
-        int middle = low + (high - low) / 2;
-        int opora = array[middle];
-
-        int i = low, j = high;
-        while (i <= j) {
-            while (array[i] < opora) {
-                i++;
-            }
-
-            while (array[j] > opora) {
-                j--;
-            }
-
-            if (i <= j) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
-            }
+        if (sortAs.compareToIgnoreCase("String") == 0) {
+            sortAsStrings(array);
+        } else if (sortAs.compareToIgnoreCase("Integer") == 0) {
+            sortAsIntegers(array, 0, array.length - 1);
+        } else {
+            throw new IllegalArgumentException();
         }
 
-        if (low < j)
-            sort(array, low, j);
 
-        if (high > i)
-            sort(array, i, high);
     }
 
-    ////////////////////////////////
-    public static String[] sortString(String[] array) {
+    /**
+     * Sorts arrray of Strings as Strings
+     *  @param array Nieposortowany ciąg obiektow.
+     */
+    public static String[] sortAsStrings(String[] array) {
         sortStrings(array, 0, array.length - 1);
         return array;
     }
@@ -59,14 +43,14 @@ public class QuickSort {
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end);
 
-            sortStrings(arr, begin, partitionIndex-1);
-            sortStrings(arr, partitionIndex+1, end);
+            sortStrings(arr, begin, partitionIndex - 1);
+            sortStrings(arr, partitionIndex + 1, end);
         }
     }
 
     private static int partition(String[] arr, int begin, int end) {
         String pivot = arr[end];
-        int i = (begin-1);
+        int i = (begin - 1);
 
         for (int j = begin; j < end; j++) {
             if (arr[j].compareTo(pivot) <= 0) {
@@ -78,10 +62,44 @@ public class QuickSort {
             }
         }
 
-        String swapTemp = arr[i+1];
-        arr[i+1] = arr[end];
+        String swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
         arr[end] = swapTemp;
 
-        return i+1;
+        return i + 1;
+    }
+
+    /**
+     * Sorts arrray of Strings as Integers
+     */
+    private static void sortAsIntegers(String[] array,int low, int high) {
+        int middle = low + (high - low) / 2;
+        int opora = Integer.parseInt(array[middle]);
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (Integer.parseInt(array[i]) < opora) {
+                i++;
+            }
+
+            while (Integer.parseInt(array[i]) > opora) {
+                j--;
+            }
+
+            if (i <= j) {
+                String temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+
+        }
+
+        if (low < j)
+            sortAsIntegers(array, low, j);
+
+        if (high > i)
+            sortAsIntegers(array, i, high);
     }
 }
